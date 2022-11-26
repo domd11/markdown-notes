b84e5e08ea073f0ac362c3501d346def3ba62323
domd11 <dominiquedesertb@gmail.com>
2022-11-25 22:21:03 -0500

    link styling

--- pages/MarkdownEditor.js
+++ pages/MarkdownEditor.js
@@ -2,6 +2,11 @@ import React, { useEffect, useState } from 'react'
 import ReactMarkdown from 'react-markdown'
 import { doc, setDoc, collection, getDocs, docs } from "firebase/firestore"
 import { db } from '../Firebase'
+import remarkGfm from 'remark-gfm';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import atomDark from 'react-syntax-highlighter/dist/cjs/styles/prism/atom-dark';
+
+
 const MarkdownEditor = ({ toggleCreate, activeNote, setToggleCreate, setNotes }) => {
 
     const [input, setInput] = useState(""); 
@@ -68,9 +73,28 @@ const MarkdownEditor = ({ toggleCreate, activeNote, setToggleCreate, setNotes })
        ) : ""}
        <button className='full-width-btn' onClick={saveNote}>Save Note</button>
         <hr />
-        <ReactMarkdown className="markdown">
-        {input}
-        </ReactMarkdown>
+        <ReactMarkdown remarkPlugins={[remarkGfm]}
+    components={{
+      code({node, inline, className, children, ...props}) {
+        const match = /language-(\w+)/.exec(className || '')
+        return !inline && match ? (
+          <SyntaxHighlighter
+            children={String(children).replace(/\n$/, '')}
+            style={atomDark}
+            language={match[1]}
+            PreTag="div"
+            {...props}
+          />
+          ) : (
+            <code className={className} {...props}>
+              {children}
+            </code>
+          )
+        }
+      }}
+     >
+   {input}
+</ReactMarkdown>
     </div>
   )
 }
